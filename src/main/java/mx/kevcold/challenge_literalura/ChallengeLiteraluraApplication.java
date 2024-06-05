28517379e6edf3579b590f2d2e8ef7578146ed71
Kevin Nuñez <seidnez@gmail.com>
2024-06-04 22:06:14 -0500

    creando archivos y paquetes iniciales

---
+++ src/main/java/mx/kevcold/challenge_literalura/ChallengeLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package mx.kevcold.challenge_literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraluraApplication.class, args);
+	}
+
+}
